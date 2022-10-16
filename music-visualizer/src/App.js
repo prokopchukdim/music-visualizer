4a2ea78d55c6ae45edf81838af60e637b803896a
prokopchukdim <87666671+prokopchukdim@users.noreply.github.com>
2022-10-16 18:09:20 -0400

    Initial setup

---
+++ music-visualizer/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div class = "circle"></div>
+        <div class="range"></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

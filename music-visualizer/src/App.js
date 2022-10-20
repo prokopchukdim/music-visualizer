975876e6b9854a873d813ae88373c5e8826b39f6
prokopchukdim <87666671+prokopchukdim@users.noreply.github.com>
2022-10-20 13:35:23 -0400

    Imported p5.js

--- music-visualizer/src/App.js
+++ music-visualizer/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import CircleContainer from './CircleContainer';
+import { ReactP5Wrapper } from 'react-p5-wrapper';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <ReactP5Wrapper sketch={CircleContainer}/>
         <div class = "circle"></div>
+        <div class = "circle-hider"></div>
         <div class="range"></div>
       </header>
     </div>
---
+++ music-visualizer/src/CircleContainer.js
@@ -0,0 +1,11 @@
+
+function CircleContainer(p){
+    p.setup = () => {
+        p.createCanvas(600,600);    
+    };
+    p.draw = () => {
+        p.background(0);
+    };
+}
+
+export default CircleContainer;

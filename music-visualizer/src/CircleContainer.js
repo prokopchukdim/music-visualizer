43f294f0dc9f945561b526076bf8e0f00dfecc97
prokopchukdim <87666671+prokopchukdim@users.noreply.github.com>
2022-10-23 16:41:06 -0400

    Visualization logic

--- music-visualizer/src/CircleContainer.js
+++ music-visualizer/src/CircleContainer.js
@@ -1,3 +1,4 @@
+import test from "./test.mp3"
 
 function CircleContainer(p){
     const angleToPoint = (angle, height) => {
@@ -21,29 +22,77 @@ function CircleContainer(p){
     
     
     var CANVAS_SIZE = 600;
-    var NUM = 16;
+    var NUM = 32;
     var ANGLE = p.TWO_PI / NUM;
     var HEIGHT = 160;
     var MAX_SCALE = 1.7;
+    var MIN_SCALE = 1.1;
     var ANGLE_SCALE = 0.75;
 
-    var randArr = []
-    var tgtArr = []
-    for (let i = 0; i<NUM; i++){
-        randArr.push(p.random(1,MAX_SCALE));
-        tgtArr.push(p.random(1,MAX_SCALE));
-    }
-    console.log(randArr);
+    // var randArr = []
+    // var tgtArr = []
+    // for (let i = 0; i<NUM; i++){
+    //     randArr.push(p.random(1,MAX_SCALE));
+    //     tgtArr.push(p.random(1,MAX_SCALE));
+    // }
+    // console.log(randArr);
     // let randScale = p.random(1, 1.6);
 
-    p.setup = () => {
+    var scaleArr = [];
+    for (let i = 0; i<NUM; i++){
+        scaleArr.push(MIN_SCALE);
+    }
+    var dataArr = [];
+    var analyserNode = null;
+    var played = false;
+
+    const setPlayed = () => {
+        played = true;
+    }
+
+    var audio = null;
+
+    const clickHandler = () => {
+        if (!audio){
+            const audioContext = new AudioContext();
+            audio = new Audio();
+            audio.src = require('./test.mp3');
+            // audio.crossOrigin = "anonymous";
+            const audioSourceNode = audioContext.createMediaElementSource(audio);
         
-        p.createCanvas(CANVAS_SIZE,CANVAS_SIZE); 
+            analyserNode = audioContext.createAnalyser();
+            analyserNode.fftSize = 64;
+            const bufferLength = analyserNode.frequencyBinCount;
+            dataArr = new Float32Array(bufferLength);
+        
+            audioSourceNode.connect(analyserNode);
+            analyserNode.connect(audioContext.destination);
+        }
+        
+        
+        if (!audio.paused){
+            audio.pause();
+        }
+
+        if (played === false){
+            audio.play();
+        }
+        
+        setPlayed();
+        
+
+    }
+
+    
+    document.addEventListener("click", clickHandler);
+
+
+    p.setup = () => {
+        let canvas = p.createCanvas(CANVAS_SIZE,CANVAS_SIZE);         
     };
 
 
     p.draw = () => {
-
         let c = p.color(221, 167, 123);
         p.fill(c);
         p.noStroke();
@@ -52,14 +101,16 @@ function CircleContainer(p){
         p.background(c);
 
         p.translate(CANVAS_SIZE/2, CANVAS_SIZE/2);
+        p.rotate(p.PI);
 
         //draw slices;
 
         for (let i = 0; i < NUM; i++){
-            p.rotate(p.TWO_PI / NUM * i)
+            // p.rotate(p.TWO_PI / NUM * i);
+            p.rotate(p.TWO_PI / NUM);
             
             
-            scale(p, randArr[i]);
+            scale(p, scaleArr[i]);
             p.beginShape();
             p.vertex(0, 0);
             p.vertex(0, HEIGHT);
@@ -68,20 +119,49 @@ function CircleContainer(p){
             drawPoint(p, ANGLE * 3 / 4 * ANGLE_SCALE);
             drawPoint(p, ANGLE * ANGLE_SCALE);
             p.endShape(p.CLOSE);
-            scale(p, 1/randArr[i]);
-            randArr.forEach( (val, ind) => {
-                if (randArr[ind] < tgtArr[ind]){
-                    randArr[ind] += 0.0004;
-                }
-                else if (randArr[ind] >= tgtArr[ind]){
-                    randArr[ind] -= 0.0004;
-                }
-                if (-0.02 < (randArr[ind] - tgtArr[ind]) && (randArr[ind] - tgtArr[ind]) < 0.02){
-                    tgtArr[ind] = p.random(1, MAX_SCALE);
-                }
-            })
-            // randArr = n;
+            scale(p, 1/scaleArr[i]);
         }
+        if (analyserNode){
+            analyserNode.getFloatFrequencyData(dataArr);
+
+            let max = Math.max(...dataArr);
+            let min = Math.min(...dataArr);
+            
+            // console.log(dataArr);
+            dataArr.forEach( (val, ind) => {
+                let dif = scaleArr[ind] - ((val - min) * (MAX_SCALE - MIN_SCALE) / (max - min) + MIN_SCALE);
+                if (dif < 0){
+                    scaleArr[ind] += Math.max(0, Math.min(-dif, 0.01));
+                }
+                else{
+                    if (scaleArr[ind] > MIN_SCALE){
+                        scaleArr[ind] += Math.min(0, Math.max(-dif, -0.01));
+                    }
+                    // console.log(Math.min(0, Math.max(-dif, -0.01)));
+                    // 
+                }
+                
+                // if (scaleArr[ind] < (val - min) * (MAX_SCALE - MIN_SCALE) / (max - min) + MIN_SCALE){
+                //     scaleArr[ind] += 0.010;
+                // }
+                // else if (scaleArr[ind] > (val - min) * (MAX_SCALE - MIN_SCALE) / (max - min) + MIN_SCALE){
+                //     scaleArr[ind] -= 0.010;
+                // }
+            });
+    
+        }
+        
+        // randArr.forEach( (val, ind) => {
+        //     if (randArr[ind] < tgtArr[ind]){
+        //         randArr[ind] += 0.005;
+        //     }
+        //     else if (randArr[ind] >= tgtArr[ind]){
+        //         randArr[ind] -= 0.005;
+        //     }
+        //     if (-0.02 < (randArr[ind] - tgtArr[ind]) && (randArr[ind] - tgtArr[ind]) < 0.02){
+        //         tgtArr[ind] = p.random(1, MAX_SCALE);
+        //     }
+        // })
         
         //Draw circles;
         p.fill(c);
